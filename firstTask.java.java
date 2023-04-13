73c5361c0cbde2395ab6f814692fcd87191a3fef
lynnweidman <54895437+lynnweidman@users.noreply.github.com>
2023-04-13 16:49:14 -0400

    Add files via upload

---
+++ firstTask.java.java
@@ -0,0 +1,11 @@
+class FirstTask {
+
+    public static void myInfo(String fName) {
+        System.out.println("Hello, my name is " + fName);
+    }
+   
+    public static void main(String[] args) {
+
+       myInfo("Lynn");
+    }
+}
